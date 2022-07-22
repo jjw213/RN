a627274d03493eccdc987d4d35835f8d18c279e7
jjw213 <ssvsxx@naver.com>
2022-07-22 18:23:47 +0900

    geolocation

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import type { Node } from 'react';
 import {
   SafeAreaView,
@@ -16,7 +16,11 @@ import {
   Text,
   useColorScheme,
   View,
+  Dimensions,
+  TouchableOpacity,
 } from 'react-native';
+
+import Geolocation from '@react-native-community/geolocation';
 
 import {
   Colors,
@@ -51,10 +55,27 @@ const Section = ({ children, title }): Node => {
     </View>
   );
 };
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLogitude] = useState(null);
 
+  const geoLocation = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        const latitude = JSON.stringify(position.coords.latitude);
+        const longitude = JSON.stringify(position.coords.longitude);
+
+        setLatitude(latitude);
+        setLogitude(longitude);
+      },
+      error => { console.log(error.code, error.message); },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    )
+  }
+  console.log(latitude);
+  const isDarkMode = useColorScheme() === 'dark';
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -66,14 +87,28 @@ const App: () => Node = () => {
         // contentInsetAdjustmentBehavior="automatic"
         style={styles.city}>
         {/* <Header /> */}
+        <Text> latitude: {latitude} </Text>
+        <Text> longitude: {longitude} </Text>
+            <TouchableOpacity onPress={() => geoLocation()}>
+                <Text> Get GeoLocation </Text>
+            </TouchableOpacity>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView pagingEnabled showsHorizontalScrollIndicator={false}
+        horizontal contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -110,10 +145,12 @@ const styles = StyleSheet.create({
     fontWeight: "500"
   },
   weather: {
-    flex: 3,
+    // flex: 3,
+    backgroundColor: "blue",
   },
   day: {
-    flex: 1,
+    // flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: "center",
     // backgroundColor: "teal",
   },
@@ -122,7 +159,7 @@ const styles = StyleSheet.create({
     fontSize: 158,
   },
   description: {
-    marginTop:-30,
+    marginTop: -30,
     fontSize: 60,
   }
 });
